b2eee8ec8000d4248fab948297b641324d3a20ef
Volkan <vlknakkaya@gmail.com>
2022-09-18 21:53:25 +0300

    prometheus and grafana were added to all services

--- email-service/src/main/java/com/hepsisurada/emailservice/EmailServiceApplication.java
+++ email-service/src/main/java/com/hepsisurada/emailservice/EmailServiceApplication.java
@@ -1,8 +1,15 @@
 package com.hepsisurada.emailservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.metrics.web.reactive.server.DefaultWebFluxTagsProvider;
+import org.springframework.boot.actuate.metrics.web.reactive.server.WebFluxTagsProvider;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.server.ServerWebExchange;
+
+import io.micrometer.core.instrument.Tag;
+import io.micrometer.core.instrument.Tags;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +19,18 @@ public class EmailServiceApplication {
 		SpringApplication.run(EmailServiceApplication.class, args);
 	}
 
+	/**
+	 * Bean to avoid requests which has UNKNOWN uri field
+	 * https://github.com/spring-projects/spring-boot/pull/15609
+	 */
+	@Bean
+	WebFluxTagsProvider webFluxTagsProvider() {
+		return new DefaultWebFluxTagsProvider() {
+			@Override
+			public Iterable<Tag> httpRequestTags(ServerWebExchange exchange, Throwable exception) {
+				return Tags.concat(super.httpRequestTags(exchange, exception), Tags.of(Tag.of("uri", exchange.getRequest().getPath().value())));
+			}
+		};
+	}
+	
 }
--- product-service/src/main/java/com/hepsisurada/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/hepsisurada/productservice/ProductServiceApplication.java
@@ -1,8 +1,15 @@
 package com.hepsisurada.productservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.metrics.web.reactive.server.DefaultWebFluxTagsProvider;
+import org.springframework.boot.actuate.metrics.web.reactive.server.WebFluxTagsProvider;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.server.ServerWebExchange;
+
+import io.micrometer.core.instrument.Tag;
+import io.micrometer.core.instrument.Tags;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +17,21 @@ public class ProductServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
+	}
+
+	/**
+	 * Bean to avoid requests which has UNKNOWN uri field
+	 * https://github.com/spring-projects/spring-boot/pull/15609
+	 */
+	@Bean
+	WebFluxTagsProvider webFluxTagsProvider() {
+		return new DefaultWebFluxTagsProvider() {
+			@Override
+			public Iterable<Tag> httpRequestTags(ServerWebExchange exchange, Throwable exception) {
+				return Tags.concat(super.httpRequestTags(exchange, exception),
+						Tags.of(Tag.of("uri", exchange.getRequest().getPath().value())));
+			}
+		};
 	}
 
 }
